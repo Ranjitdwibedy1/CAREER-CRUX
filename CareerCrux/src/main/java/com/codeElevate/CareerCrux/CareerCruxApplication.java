974b5fa7d44fe657ee9531d601c653d833e18f80
Delta Student <dwibedyranjit@gmail.com>
2024-11-07 12:19:30 +0530

    initial commit

---
+++ CareerCrux/src/main/java/com/codeElevate/CareerCrux/CareerCruxApplication.java
@@ -0,0 +1,13 @@
+package com.codeElevate.CareerCrux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerCruxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerCruxApplication.class, args);
+	}
+
+}
---
+++ CareerCrux/src/main/java/com/codeElevate/CareerCrux/entity/TestClass.java
@@ -0,0 +1,11 @@
+package com.codeElevate.CareerCrux.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestClass {
+
+    @Id
+    private Long id;
+}
